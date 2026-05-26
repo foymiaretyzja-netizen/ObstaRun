442b8fd386ec016738d2414a3a76eaf89fa34b82
foymiaretyzja-netizen <foymiaretyzja@gmail.com>
2026-05-26 15:30:30 -0500

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,7 @@
+export const GRID_SIZE = 30;
+export const GRAVITY = 0.6;
+export const FRICTION = 0.8;
+export const JUMP_POWER = -14;
+export const HEAL_ORB_VALUE = 25;
+export const BOSS_BULLET_DAMAGE = 10;
+// ... export the rest of your constants
