4743b494820148692d1d3f2d7c21049a8eedcf9d
foymiaretyzja-netizen <foymiaretyzja@gmail.com>
2026-05-27 09:37:11 -0500

    Update physics parameters for terminal velocity and friction

--- js/constants.js
+++ js/constants.js
@@ -3,7 +3,7 @@ export const GRID_SIZE = 32;
 
 // Physics parameters brought directly over from infinite_lava_run.html
 export const GRAVITY = 0.6;
-export const TERMINAL_VELOCITY = 2;
-export const FRICTION = 0.8;
+export const TERMINAL_VELOCITY = 7;
+export const FRICTION = 3;
 export const BASE_JUMP_FORCE = -12;
 export const BASE_ACCEL = .2;
