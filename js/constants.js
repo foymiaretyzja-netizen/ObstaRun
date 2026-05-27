61dc756ec456b13fd44977dc25062d313c91072a
foymiaretyzja-netizen <foymiaretyzja@gmail.com>
2026-05-27 09:35:26 -0500

    Change BASE_ACCEL value from 1 to 0.2

--- js/constants.js
+++ js/constants.js
@@ -6,4 +6,4 @@ export const GRAVITY = 0.6;
 export const TERMINAL_VELOCITY = 12;
 export const FRICTION = 0.8;
 export const BASE_JUMP_FORCE = -12;
-export const BASE_ACCEL = 1;
+export const BASE_ACCEL = .2;
